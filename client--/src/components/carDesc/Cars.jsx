a48654b7210505aa0e91157ac4bb612fc50f40b2
toum21 <pey2moo1@gmail.com>
2024-01-11 14:16:01 -0800

    updated page layout and styling a little more

--- client--/src/components/carDesc/Cars.jsx
+++ client--/src/components/carDesc/Cars.jsx
@@ -1,11 +1,11 @@
-
 import Card from 'react-bootstrap/Card';
 
 function Cars() {
     return (
         <div className="container my-4">
             <div className="row justify-content-center">
-                <div className="col-md-6">
+                {/* nissan 350z */}
+                <div className="col-md-6 mb-4">
                     <Card style={{ background: '#1a1a1a', color: 'white' }}>
                         <Card.Img
                             variant="top"
@@ -19,7 +19,8 @@ function Cars() {
                         </Card.Body>
                     </Card>
                 </div>
-                <div className="col-md-6">
+                {/* poresche */}
+                <div className="col-md-6 mb-4">
                     <Card style={{ background: '#1a1a1a', color: 'white' }}>
                         <Card.Img
                             variant="top"
@@ -29,6 +30,42 @@ function Cars() {
                         <Card.Body>
                             <Card.Text style={{ maxHeight: '150px', overflow: 'hidden' }}>
                                 The Porsche 911 GT3 is a high-performance homologation model of the Porsche 911 sports car. It is a line of high-performance models, which began with the 1973 911 Carrera RS. The GT3 has had a successful racing career in the one-make national and regional Porsche Carrera Cup and GT3 Cup Challenge series, as well as the international Porsche Supercup supporting the FIA F1 World Championship.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                {/* benz truck */}
+                <div className="col-md-6 mb-4">
+                    <Card style={{ background: '#1a1a1a', color: 'white' }}>
+                        <Card.Img
+                            variant="top"
+                            src="https://www.usatoday.com/gcdn/-mm-/fd55f3615a822abaace32e9c793bfb122480bc05/c=1219-2063-5737-4616/local/-/media/2017/03/01/USATODAY/USATODAY/636239785468297609-mb1.jpg"
+                            style={{ height: '300px', objectFit: 'cover' }}
+                        />
+                        <Card.Body>
+                            <Card.Text style={{ maxHeight: '150px', overflow: 'hidden' }}>
+                                Daimler, parent of Mercedes-Benz, says it is bringing the latest concept version of its first pickup to the Geneva Motor Show later this month as a luxury midsize model.
+
+                                When it hits showrooms later this year, X-Class will be sold in what Mercedes says are a bunch of "key markets," and the U.S. isn't included (for the moment). They are Argentina, Brazil, South Africa, Australia with New Zealand and Europe.
+
+                                "With the Mercedes-Benz pickup, we will close one of the last gaps in our portfolio," said Dieter Zetsche, Daimler's chairman and head of Mercedes-Benz Cars. "The X-Class will set new standards in a growing segment."
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                {/* skyline */}
+                <div className="col-md-6 mb-4">
+                    <Card style={{ background: '#1a1a1a', color: 'white' }}>
+                        <Card.Img
+                            variant="top"
+                            src="https://cdn.motor1.com/images/mgl/2NZgLx/s1/nissan-gt-r-r32-ev-conversion.jpg"
+                            style={{ height: '300px', objectFit: 'cover' }}
+                        />
+                        <Card.Body>
+                            <Card.Text style={{ maxHeight: '150px', overflow: 'hidden' }}>
+                                Nissan took many by surprise at the end of last month when it announced plans to convert a GT-R from the R32 generation into an electric vehicle. A new walkaround video published on Twitter by the company's Japanese division fully reveals the sports car in its current state, right before losing the oily bits. As with all Skyline models produced during the 1989-1993 era, it's a right-hand-drive example, and this one comes in the two-door body style.
+
+                                The R32 EV project is still in its infancy and will come to life following an engineer's idea to give the classic Skyline an electric powertrain. The higher-ups at Nissan approved the build, which will involve yanking off the RB26, a twin-turbo inline-six engine that made 276 horsepower (206 kilowatts) and 271 pound-feet (368 Newton-meters) of torque. Well, at least on paper.
                             </Card.Text>
                         </Card.Body>
                     </Card>
--- client--/src/components/firstArea/Firstarea.jsx
+++ client--/src/components/firstArea/Firstarea.jsx
@@ -1,10 +1,9 @@
-
 import Figure from 'react-bootstrap/Figure';
 
 function FirstArea() {
     return (
         <div className="container">
-            <Figure className="my-5">
+            <Figure className="my-5 d-flex flex-column align-items-center justify-content-center text-center">
                 <Figure.Image
                     width={1000} 
                     height={400} 
@@ -12,7 +11,7 @@ function FirstArea() {
                     src="https://www.bmw-special-sales.com/content/dam/bmw/marketBMWCOM/bmw-special-sales_com/teaser-pool/medium/home/corporate2-sales.jpg"
                     fluid 
                 />
-                <Figure.Caption className="text-center">
+                <Figure.Caption>
                     The BMW eDrive technology in a plug-in hybrid combines three components to the cleanest and most efficient way of driving: an innovative electric drive, a lithium-ion high-performance battery, and an intelligent energy management. It will always choose the ideal driving mode for the best efficiency depending on battery charge level, speed, and route. Driving in the all-electric mode and with full-electric models like BMW iX, BMW i4, BMW iX3, or BMW i3/i3s is locally emission-free – and provides an impressive acceleration.
                 </Figure.Caption>
             </Figure>
