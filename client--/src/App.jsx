5a5f2ea906b6450083273fe7d258bf03d8706e5e
toum21 <pey2moo1@gmail.com>
2024-01-11 12:12:20 -0800

    added bootstrap and react-bootstrap and used that to create a nav bar

--- client--/src/App.jsx
+++ client--/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import Header from './components/header/Header'
 import FirstArea from './components/firstArea/Firstarea'
+import Topbar from './components/navbar/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {  
   return (
     <>
+    <Topbar />
     <Header />
     <FirstArea />
     </>    
---
+++ client--/src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function TopBar() {
+    return (
+        <Navbar bg="dark" data-bs-theme="dark">
+            <Container>
+                <Navbar.Brand href="#home">BuyACar</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Sale Of The Day</Nav.Link>
+                    <Nav.Link href="#features">About Us</Nav.Link>
+                    <Nav.Link href="#pricing">Contact Us</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default TopBar;
