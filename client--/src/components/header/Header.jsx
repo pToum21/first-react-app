6733ca47a3682478e9f8f4f250a98d794235094f
toum21 <pey2moo1@gmail.com>
2024-01-10 20:15:52 -0800

    added an image to the header with inline styling

--- client--/src/components/header/Header.jsx
+++ client--/src/components/header/Header.jsx
@@ -1,11 +1,31 @@
-import './header.css'
+import './header.css';
+
+const containerStyle = {
+    display: "flex",
+    justifyContent: "space-between", 
+    alignItems: "center",
+    textAlign: "center",
+}
+
+const textContainerStyle = {
+    textAlign: "left", 
+}
+
+const imgStyle = {
+    width: "175px",
+    height: "auto",
+    boxShadow: "0 0 10px 5px rgba(255, 255, 255, 0.7)",
+    opacity: 0.6,
+}
 
 function Header() {
     return (
-        <div className='full-header'>
-            <h1 className="header-title">Buy a Car Today!</h1>
-            <p className="header-desc">with the best prices on the west coast, buyaCar meets all your needs</p>
-            <img src="https://www.shutterstock.com/image-vector/car-shop-shopping-logo-design-260nw-574467076.jpg" alt="buy a car logo" />
+        <div className='full-header' style={containerStyle}>
+            <div style={textContainerStyle}>
+                <h1 className="header-title">Buy a Car Today!</h1>
+                <p className="header-desc">With the best prices on the west coast, BuyaCar meets all your needs</p>
+            </div>
+            <img style={imgStyle} src="https://www.shutterstock.com/image-vector/car-shop-shopping-logo-design-260nw-574467076.jpg" alt="buy a car logo" />
         </div>
     )
 }
