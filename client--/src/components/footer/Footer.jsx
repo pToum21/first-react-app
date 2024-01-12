21445f130eea69ec1fb5cba03059f29de4937cc8
toum21 <pey2moo1@gmail.com>
2024-01-11 22:33:05 -0800

    updated the footer to look cleaner

--- client--/src/components/footer/Footer.jsx
+++ client--/src/components/footer/Footer.jsx
@@ -1,18 +1,20 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import TopBar from '../navbar/Navbar';
+
 
 function Footer() {
     return (
         <Card className="text-center" bg="dark" expand="lg" variant="dark" text="white">
             <Card.Body>
-                <Card.Title>Contact Us</Card.Title>
-                <Card.Text>
-                    {/* Add your text content here */}
-                </Card.Text>
-                <Button variant="outline-light" href="#nav" className="text-decoration-none">
-                    Top of Page
-                </Button>
+                <div className="d-flex justify-content-around align-items-center flex-wrap">
+                    <Button variant="outline-light" href="#contact" className="text-decoration-none mb-2">
+                        Contact Us
+                    </Button>
+                    <p>ViewACar</p>
+                    <Button variant="outline-light" href="#nav" className="text-decoration-none mb-2">
+                        Top of Page
+                    </Button>
+                </div>
             </Card.Body>
             <Card.Footer className="text-white">
                 View A Car is designed to allow users to view popular cars and cool cars at the time. Any request can be sent via email to have it uploaded.
